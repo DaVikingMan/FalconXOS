97727d7b7eb4514e654b774401ac26cd39f53c0a
DaVikingMan <shashwatsinha080@gmail.com>
2021-03-14 00:24:58 +0530

    Main Class

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -211,6 +211,8 @@ namespace Classes
                             }
                             else if(Tread == "MainTerminal")
                             {
+                                CommandAllReplica l = new CommandAllReplica();
+                                l.MainTerminalRe();
 
                             }
                             
@@ -240,6 +242,7 @@ class CommandAllReplica
         Console.WriteLine("Welcome to Note! The place where you can learn how to use TerminalX");
         
         Console.WriteLine("What Note do you want to read?");
+        Console.WriteLine("Exit");
         Console.Write("> ");
         var readInput = Console.ReadLine();
 
@@ -254,13 +257,92 @@ class CommandAllReplica
     }
     public void StartRe() // Start Replica
     {
-        var m = new viOne();
-        m.Start();
+        Console.Clear();
+            DateTime date = DateTime.Now;
+                        Console.WriteLine(date);
+                        Console.ReadKey();
+                        
+                        Console.WriteLine("Welcome to the main system!");
+                        Console.WriteLine("Kindly write your name : ");
+                        Console.Write("> ");
+                        string name = Console.ReadLine();
+                        if(string.IsNullOrWhiteSpace(name)){
+                            Console.WriteLine("Kindly input name");
+                            Console.ReadKey();
+                            StartRe();
+                        }
+                        
+                        Console.Clear();
+                        Console.WriteLine(name  + " huh?");
+                        Console.ReadKey();
+                        
         Console.ReadKey();
         Console.WriteLine("Virtualization is carried by Re:Virtual");
         Console.WriteLine("");
         Console.ReadKey();
+        Console.WriteLine("Kindly type Exit");
+        Console.Write(">");
+        string n = Console.ReadLine();
+        if(n == "Exit")
+        {
+            viOne v = new viOne();
+            v.vi();
+
+
+        }
+
         
+    }
+    public void MainTerminalRe()
+    {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.Clear();
+            Console.WriteLine("Welcome to a new version of TerminalX");
+            Console.WriteLine("Do you want to read about TerminalX and its features?");
+            Console.Write("> ");
+            var c = Console.ReadLine();
+            if(c == "Yes")
+            {
+                
+                NotesRe();
+                Console.ReadKey();
+        Console.WriteLine("Virtualization is carried by Re:Virtual");
+        Console.WriteLine("");
+        Console.ReadKey();
+        Console.WriteLine("Kindly type Exit");
+        Console.Write(">");
+        string n = Console.ReadLine();
+        if(n == "Exit")
+        {
+                  viOne m = new viOne();
+                  m.vi();
+
+        }
+
+            }else if(c == "No")
+            {
+                Console.Clear();
+                Console.ReadKey();
+                Console.WriteLine("This version includes many new features and some old algorithms such as /.Bait the system./");
+                Console.WriteLine("The available versions are as follows : ");
+                Console.WriteLine("vi.1");
+                Console.ReadKey();
+                Console.Clear();
+                Console.WriteLine("What version do you want to start?");
+                Console.ReadKey();
+        Console.WriteLine("Virtualization is carried by Re:Virtual");
+        Console.WriteLine("");
+        Console.ReadKey();
+        Console.WriteLine("Kindly type Exit");
+        Console.Write(">");
+        string n = Console.ReadLine();
+        if(n == "Exit")
+        {
+                 viOne a = new viOne();
+                 a.vi();
+
+        }
+            }
     }
 
 }
