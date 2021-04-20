d3808927addc356b8f8f48243dd63dc407bf7235
DaVikingMan <shashwatsinha080@gmail.com>
2021-04-20 16:06:20 +0530

    Change kernel

---
+++ src/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Classes
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            
+            
+            
+            kernel k = new kernel();
+
+            k.KernelMain();
+            
+        }
+    }
+}
