2b03d86bf01404ef0349999995ab607df87879d0
DaVikingMan <shashwatsinha080@gmail.com>
2021-04-20 16:04:11 +0530

    Create Property

---
+++ src/commands.cs
@@ -0,0 +1,16 @@
+namespace Classes
+{
+    // Initializes commands
+    public class commands{
+       
+            public string command1 { get; set; }
+            public string command2 { get; set; }
+            public string command3 { get; set; }
+            public string command4 { get; set; }
+            
+            
+
+        
+    
+    }
+}
