5e17147c1a0d1efb8c528e81ebb2cf5dfce8e50c
DaVikingMan <shashwatsinha080@gmail.com>
2021-04-20 16:06:01 +0530

    Change Arguments

---
+++ src/ProcessHandler.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Diagnostics;
+
+namespace Classes{
+
+    class ProcessHandler
+    {
+        public void ProcessHandle()
+        {
+           ProcessStartInfo startInfo = new ProcessStartInfo();    
+            string executeCommand = "java MainMaker.java";
+
+            Process proc = new Process();
+            
+            startInfo.CreateNoWindow = true;
+            startInfo.UseShellExecute = false;
+            startInfo.FileName = "CMD.exe";
+            startInfo.Arguments = "/c java MainMaker.java";
+            
+
+            
+            
+            
+            
+           Process.Start(startInfo);
+        }
+    }
+}
