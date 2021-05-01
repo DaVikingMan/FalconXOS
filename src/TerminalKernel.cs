9d8e4307af313c3d8ef1faa100ef2824fd6c714b
DaVikingMan <shashwatsinha080@gmail.com>
2021-05-01 20:13:53 +0530

    Add ProcessHandler for Terminal Execution

---
+++ src/TerminalKernel.cs
@@ -0,0 +1,23 @@
+using System.Diagnostics;
+using System;
+using System.Collections.Generic;
+
+namespace src
+{
+    class TerminalKernel
+    {
+        public void TerminalHandler()
+        {
+            Console.WriteLine("Define Starting Directory(full path)");
+            string w = Console.ReadLine();
+            ProcessStartInfo m = new ProcessStartInfo();
+            
+            m.WorkingDirectory = w;
+            m.UseShellExecute = true;
+            m.CreateNoWindow = false;
+            m.FileName = "powershell.exe";
+            Process.Start(m);
+        }
+    }
+    
+}
