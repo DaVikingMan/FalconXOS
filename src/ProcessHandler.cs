8acc6bb4e50b8c3a3e94b5def58a0834e60f724f
DaVikingMan <shashwatsinha080@gmail.com>
2021-05-01 20:13:19 +0530

    Fix Process

--- src/ProcessHandler.cs
+++ src/ProcessHandler.cs
@@ -5,16 +5,19 @@ namespace src{
 
     class ProcessHandler
     {
+        
         public void ProcessHandle()
         {
-           ProcessStartInfo startInfo = new ProcessStartInfo();    
-            string executeCommand = "java MainMaker.java";
+           
+            ProcessStartInfo startInfo = new ProcessStartInfo();    
+            
 
             Process proc = new Process();
-            
+           
             startInfo.CreateNoWindow = true;
             startInfo.UseShellExecute = false;
             startInfo.FileName = "CMD.exe";
+           
             startInfo.Arguments = "/c java MainMaker.java";
             
 
