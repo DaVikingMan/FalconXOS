1b41e70ce947678958376b755bd5bccbb911ae90
DaVikingMan <shashwatsinha080@gmail.com>
2021-05-04 22:32:14 +0530

    Update Kernel

--- src/kernel.cs
+++ src/kernel.cs
@@ -11,16 +11,16 @@ namespace src
         {
 
             Console.ForegroundColor = ConsoleColor.DarkCyan;
-            Console.Clear();
+            Console.Clear(); // Clears the console
             
             Console.WriteLine("Type start -oss n to start a Operating Shell session");
             Console.Write(">");
             string commandStart = Console.ReadLine();
-            if(commandStart == "start -oss n")
+            if(commandStart == "start -oss n") // When the listed command is executed
             {
                 viOne v = new viOne();
                 v.Start();
-            }else 
+            }else  // When the wrong command is executed
             {
                 Console.WriteLine("Wrong command");
                 Console.ReadKey();
