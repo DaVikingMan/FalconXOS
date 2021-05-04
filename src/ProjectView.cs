a0446bde99370c810e41b6b8f490dd44cb9a4251
DaVikingMan <shashwatsinha080@gmail.com>
2021-05-04 22:33:02 +0530

    Add View Project sub-utility

---
+++ src/ProjectView.cs
@@ -0,0 +1,36 @@
+using System;
+using System.IO;
+
+namespace src{
+    class ProjectView
+    {
+        public void View()
+        {
+            Console.WriteLine("Type scc -- view");
+            Console.Write(">");
+            string Bcommand = Console.ReadLine();
+            if(Bcommand == "scc --view")
+            {
+               Console.WriteLine("Type the filePath where your project to-do list is stored");
+               Console.Write(">");
+               string yCommand = Console.ReadLine();
+               string filePath = $@"{yCommand}";
+               Console.Clear();
+               var t = File.ReadAllText(filePath);
+               Console.WriteLine(t);
+               Console.WriteLine("This was your list's to-do");
+               Console.WriteLine("Type Exit or Stay");
+               string hcommand = Console.ReadLine();
+               if(hcommand == "Exit")
+               {
+                   Utility m = new Utility();
+                   m.VirtualUtility();
+               }else if(hcommand == "Stay")
+               {
+                   View();
+               }
+                  
+            }
+        }
+    }
+}
