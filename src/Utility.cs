d29bc0d038982579c21c42eeaa5f43932929fea7
DaVikingMan <shashwatsinha080@gmail.com>
2021-05-05 12:26:47 +0530

    Add FilePathG

--- src/Utility.cs
+++ src/Utility.cs
@@ -3,20 +3,21 @@ using System;
 namespace src 
 {
     class Utility{
-        public void VirtualUtility()
+        public void VirtualUtility(string Yfile)
         {
+            string filePathg = $@"{Yfile}";
             Console.Clear();
-            Console.WriteLine("Type 'sc -add' for creating a new project, 'sc -access' for adding the to-fo list in the project, 'sc --viewp' for viewing the to-do list and '/command Exit' for exiting the utility");
+            Console.WriteLine("Type 'sc -add' for creating a new project,\n 'sc -access' for adding the to-fo list in the project,\n 'sc --viewp' for viewing the to-do list and '/command Exit' for exiting the utility");
             Console.Write(">");
             string co = Console.ReadLine();
             if(co == "sc -add")
             {
                 ProjectManager k = new ProjectManager();
-                k.Manager();
+                k.Manager(filePathg);
             }else if(co == "sc -access")
             {
                 AccessUtlity b = new AccessUtlity();
-                b.AccessProject();
+                b.AccessProject(filePathg);
             }else if(co == "/command Exit")
             {
                 Menu jmn = new Menu();
@@ -24,7 +25,7 @@ namespace src
             }else if(co == "sc --viewp")
             {
                 ProjectView j = new ProjectView();
-                j.View();
+                j.View(filePathg);
             }
             
         }
