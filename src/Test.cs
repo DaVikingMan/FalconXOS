7b36d708bdd5e8b8531beeaf10d8da3ece4e8e68
DaVikingMan <shashwatsinha080@gmail.com>
2021-05-11 13:57:52 +0530

    Add Test

---
+++ src/Test.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Threading;
+
+namespace src
+{
+    class Test
+    {
+        public void TestMethod() // This is the basic template
+        {
+            Thread.Sleep(100);
+            viOne v = new viOne();
+            v.vi();
+
+        }
+    
+        
+    }
+}
