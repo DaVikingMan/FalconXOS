73120b1715814a3a50c802df01c903d5be61a37a
DaVikingMan <shashwatsinha080@gmail.com>
2021-05-18 20:52:13 +0530

    Add 'start /terminal'

--- src/kernel.cs
+++ src/kernel.cs
@@ -3,18 +3,19 @@ using System.Diagnostics.Tracing;
 using System.Diagnostics;
 using System.Threading;
 
-namespace src
+namespace src // Main namespace
 {
-    // This is the first class that loads when you run the program
-    class kernel
+    
+    class kernel // Starting class
     {
-        public void KernelMain()
+        public void KernelMain() // Main class
         {
+            Console.Title = "TerminalXOS"; // Sets the title
 
-            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.ForegroundColor = ConsoleColor.DarkCyan; // Sets the colour to the specified colour
             Console.Clear(); // Clears the console
             
-            Console.WriteLine("Type start -oss n to start a Operating Shell session");
+            Console.WriteLine("Type start -oss n to start the Operating Shell");
             Console.Write(">");
             string commandStart = Console.ReadLine();
             if(commandStart == "start -oss n") // When the listed command is executed
@@ -38,12 +39,28 @@ namespace src
                 DebugManager manager = new DebugManager();
                 manager.Debug();
                 
+            }else if(commandStart == "/skip shortcut")
+            {
+                Shortcut shortcut = new Shortcut();
+                shortcut.ShortcutPage();
+
+            }else if(commandStart == "/skip submenu")
+            {
+                viOne v = new viOne();
+                v.vi();
+
+            }else if(commandStart == "start /terminal")
+            {
+                close closeterminal = new close();
+                closeterminal.closeCommand();
+                
+
             }
             
             else  // When the wrong command is executed
             {
                 Console.WriteLine("Wrong command");
-                Thread.Sleep(100);
+                Thread.Sleep(100); // Waits for 100 milliseconds
                 KernelMain();
             }
           
