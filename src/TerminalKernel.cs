d4c44ee6066fb257d3f5628dc4a9d1003fdee400
DaVikingMan <shashwatsinha080@gmail.com>
2021-06-23 21:47:24 +0530

    Add Comments

--- src/TerminalKernel.cs
+++ src/TerminalKernel.cs
@@ -4,18 +4,18 @@ using System.Collections.Generic;
 
 namespace src
 {
-    class TerminalKernel
+    class TerminalKernel // Opens the specified App
     {
         public void TerminalHandler()
         {
             Console.WriteLine("Define Starting Directory(full path)");
-            string w = Console.ReadLine();
+            string w = Console.ReadLine(); // Defines in which directory to open the terminal
             ProcessStartInfo m = new ProcessStartInfo();
             
             m.WorkingDirectory = w;
             m.UseShellExecute = true;
             m.CreateNoWindow = false;
-            m.FileName = "powershell.exe";
+            m.FileName = "powershell.exe"; // Opens the specified terminal in working directory 'w'
             Process.Start(m);
 
             
