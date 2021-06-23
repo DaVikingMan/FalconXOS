cd73c800d18bb7c39ee538d65b23bf452f96e2cd
DaVikingMan <shashwatsinha080@gmail.com>
2021-06-23 21:43:01 +0530

    Add Comments

--- src/DebugManager.cs
+++ src/DebugManager.cs
@@ -5,24 +5,25 @@ using System.Threading;
 
 namespace src
 {
-    public class DebugManager
+    public class DebugManager // This class is executed when you run the 'Debug' command in the submenu
     {
         public void Debug()
         {
+            Console.ForegroundColor = ConsoleColor.Cyan;
             Console.Clear();
-            Console.WriteLine("Checking :: ");
-            if(File.Exists(@$"{Environment.CurrentDirectory}\debug\Test.cs"))
+            Console.WriteLine("Checking :: "); // Checks if 'Test.cs' exists
+            if(File.Exists(@$"{Environment.CurrentDirectory}\debug\Test.cs")) 
             {
-                Console.WriteLine("'Test.cs' Exists");
+                Console.WriteLine("'Test.cs' Exists"); // Displays this if the file exists
 
             }else 
             {
-                Console.WriteLine("Doesnt exist");
+                Console.WriteLine("Doesnt exist"); // If the file doesn't exist
             }
             Thread.Sleep(1000);
             Console.WriteLine("This method launches the Test class");
-            Console.WriteLine("Add all the classes which you want to run in the test class(if you want to run an app of a different programming language then you can try the 'Process' method in C#)");
-            Console.WriteLine("Initializing and running 'Test.cs'");
+            Console.WriteLine("Add all the classes which you want to run in the test class(Test.cs)(if you want to run an app of a different programming language then you can try the 'Process' method in C#)");
+            Console.WriteLine("Running 'Test.cs'");
             Thread.Sleep(2000);
             Test test = new Test();
             test.TestMethod();
