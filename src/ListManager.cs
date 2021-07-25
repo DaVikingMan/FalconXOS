42546ce0b3b868bc3e1e9ae385f4380cfe1271d5
DaVikingMan <shashwatsinha080@gmail.com>
2021-07-25 14:33:02 +0530

    Remove 'command5'

--- src/ListManager.cs
+++ src/ListManager.cs
@@ -10,7 +10,7 @@ namespace src
 
             List<commands> c = new List<commands>(); // Creates a new instance of the 'commands' class
 
-            c.Add(new commands{command1 = "UFT Info", command2 = "ListMaker", command3 = "WritePad", command4 = "Run" , command5 = "Exit"}); // Fills the commands with values
+            c.Add(new commands{command1 = "UFT Info", command2 = "ListMaker", command3 = "WritePad", command4 = "Exit"}); // Fills the commands with values
 
             return c; // returns List c
 
