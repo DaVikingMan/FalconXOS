3596fec422d0740d319fee73adf1aaf702c51983
DaVikingMan <shashwatsinha080@gmail.com>
2021-07-25 14:33:50 +0530

    Create QuickRun

---
+++ src/QuickRun.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Diagnostics;
+
+namespace src{
+    class QuickRun
+    {
+        public void QuickRunMethod()
+        {
+            Console.Clear();
+            Console.WriteLine("Type the name of a runnable file or the path of it");
+            Console.Write(">");
+            string readRunnable = Console.ReadLine();
+            if(readRunnable == "/command Exit")
+            {
+                var m = new Menu();
+                m.MenuMain();
+                QuickRunMethod();
+            }
+            else{
+                RunFile(readRunnable);
+                QuickRunMethod();
+            }
+            
+            
+        }
+        public void RunFile(string path)
+        {
+            var start = new ProcessStartInfo();
+            start.Arguments = $"Start-process '{path}'";
+            start.UseShellExecute = false;
+            start.FileName = "powershell.exe";
+            start.CreateNoWindow = true;
+            Process.Start(start);
+        }
+    }
+}
