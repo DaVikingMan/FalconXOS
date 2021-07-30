1bc6f28bc3c88e673bb5487b6984fe2b519b38f7
DaVikingMan <shashwatsinha080@gmail.com>
2021-07-30 21:14:38 +0530

    Add process.exit()

--- debug/Test.js
+++ debug/Test.js
@@ -6,11 +6,12 @@ var read = readline.createInterface({
     output : stdout
 });
 console.log("Running file ::");
+// Add the code which you want to test here
 read.question('Exit?', (answer) =>
 {
      console.log(answer);
      if(answer == "Exit")
      {
-         process.exit(0);
+         process.exit();
      }
 })
