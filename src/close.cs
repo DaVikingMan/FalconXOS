6d034af4cda97c2fc81e16fa6066f88db696a78e
DaVikingMan <shashwatsinha080@gmail.com>
2021-08-02 22:06:39 +0530

    Add bash

--- src/close.cs
+++ src/close.cs
@@ -15,7 +15,7 @@ namespace src
             m.WorkingDirectory = w;
             m.UseShellExecute = true;
             m.CreateNoWindow = false;
-            m.FileName = "powershell.exe";
+            m.FileName = "/bin/bash";
             Process.Start(m);
             Thread.Sleep(1000);
             Console.WriteLine("Exiting:");
