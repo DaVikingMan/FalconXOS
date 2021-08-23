9b5afd740955188526e441a2035b3ad455ebc3f0
DaVikingMan <shashwatsinha080@gmail.com>
2021-08-24 00:28:42 +0530

    Add properties

---
+++ src/ListMaintain/error.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace src
+{
+    class error
+    {
+        public string Error1 { get; set; }
+        public string Error2 { get; set; }
+        public string Error3 { get; set; }
+        public string Error4 { get; set; }
+        public string FalconError { get; set; }
+    }
+}
