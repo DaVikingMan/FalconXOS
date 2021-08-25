4ba6b9a815bed66eaff4339a5e5d046eb9d5be18
DaVikingMan <shashwatsinha080@gmail.com>
2021-08-25 22:07:34 +0530

    Remove MainChangelog(); This line had caused RunChangelog to execute '--access ch' in an infinite loop >-<

--- src/RunChangelog.cs
+++ src/RunChangelog.cs
@@ -8,14 +8,16 @@ namespace src
         public void MainChangelog()
         {
             ProcessStartInfo startInfo = new ProcessStartInfo();
-                startInfo.WorkingDirectory = $"{Environment.CurrentDirectory}/Changelog";
-                startInfo.Arguments = "/C node changelog.js";
+            Console.WriteLine(Environment.CurrentDirectory);
+            Console.ReadKey();
+                startInfo.WorkingDirectory = $@"{Environment.CurrentDirectory}/Changelog";
                 startInfo.FileName = "CMD.exe";
+		        startInfo.Arguments = "/C node changelog.js";
                 startInfo.UseShellExecute = true;
-                startInfo.CreateNoWindow = false;
+                startInfo.CreateNoWindow = true;
                 Process.Start(startInfo);
-                Console.WriteLine("Exiting  : ");
-                MainChangelog();
+               
+                
         }
     }
 }
