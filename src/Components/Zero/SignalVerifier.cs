33846e78629cb322549701738446bacc01a46082
DaVikingMan <shashwatsinha080@gmail.com>
2021-09-10 22:31:09 +0530

    Add 'SignalVerifier' for verifying signals This class is used to tell if the signal is signed or unsigned Signed-off by : DaVikingMan

---
+++ src/Components/Zero/SignalVerifier.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Diagnostics;
+
+/*
+       '#' means the signal is unsigned.
+       If you want to sign a signal, add a '!', this means that the signal is signed
+       Although this is the default rules for signed and unsigned signals, you can add your own rules for signing signals in SignalSign
+
+       The feature signed signals and unsigned signals was added to differ between signals you want to use or are complete and unsigned signals indicate that the signal is a new signal made by a user or is not complete.
+       For example, if you are working on a signal and think that it is not complete you can add a '#' symbol for making it unsigned.
+       Unsigned signals also don't display messages and are not reported by Shiro  if any errors are there.
+       This allows you to develop a new error system for your signals more easily
+      
+      It is recommended to either sign or unsign the signal
+*/
+
+namespace src
+{
+    class SignalVerifier
+    {
+        
+        public void Verify(string filetoverify, int errorsignal)
+        {
+            var zero = new ZeroSignal();
+            Console.WriteLine(filetoverify);
+            Console.WriteLine(errorsignal);
+            var signal = new SignalReceiver();
+            string verifysign = filetoverify;
+            if(verifysign.Contains('#'))
+            {
+                signal.sendsignal("Unsigned", verifysign, errorsignal);
+            }else if(verifysign.Contains('!'))
+            {
+                var errortunnel = new Errortunnel();
+                
+                errortunnel.AddInfo("/", errorsignal);
+                zero.ReceiveSignal(filetoverify);
+
+                
+            }else
+            {
+                Console.WriteLine("Error : The signal is neither signed nor unsigned");
+                Console.WriteLine("Not acceptable signal, force exiting FalconXOS");
+                Environment.Exit(0);           
+            }
+        }
+        public void SignalSign(int signedstring)
+        {
+            
+                 // This method can be used to add your own rules for signing signals
+        }
+        public void SignalErrorMaintainer(uint unsignedstring)
+        {
+                // This is to send a signal to 'Shiro' for error handling
+        }
+
+    }
+}
