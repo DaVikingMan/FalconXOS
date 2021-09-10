92f09886ff64e4db0c3d921b19462b9ae24616d8
DaVikingMan <shashwatsinha080@gmail.com>
2021-09-10 23:05:03 +0530

    Create 'ErrorInfo.cs' and add info about error 1 This file is not complete and will be completed in the next version Signed-off by : DaVikingMan

---
+++ src/Components/Shiro/Crowned/ErrorInfo.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace src
+{
+    class ErrorInfo
+    {
+        public void ErrorInfoContainer(int erroruint)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+           
+               // This class seems empty because it hasn't been finshed, it should be completed in the next release
+
+             if(erroruint == 20)
+             {
+                 
+                  Console.WriteLine("The input stream couldn't find a text that matches your input");
+                  
+                  
+           
+             }
+             
+
+                         
+        }
+    }
+}
