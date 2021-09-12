151aba1fa0bfcfe1f9a72d692f058b919c2bd946
DaVikingMan <shashwatsinha080@gmail.com>
2021-09-12 17:06:45 +0530

    Add '!' to 'Give version' and remove 'SendSignal()' Signed-off by : DaVikingMan

--- src/Components/Zero/ZeroSignal.cs
+++ src/Components/Zero/ZeroSignal.cs
@@ -16,10 +16,10 @@ namespace src
                     Console.WriteLine("Exiting :: ");
                     Thread.Sleep(2000);
                     Environment.Exit(0);
-                }else if(receivestring == "Give version")
+                }else if(receivestring == "Give version!")
                 {
                     Console.WriteLine("Received ", receivestring);
-                        Console.WriteLine("Version : .16.2-Windows(Falcon)");
+                        Console.WriteLine("Version : .19.4-Windows(Falcon)");
                         Console.WriteLine("Exiting :: ");
                         Thread.Sleep(5000);
                 }else if(receivestring == "Request debug info!")
@@ -30,14 +30,6 @@ namespace src
                     Console.Write(MemoryWrite);
                 }
         }
-        public void SendSignal(string sendstring, string optionstring) // This is not necessary
-        {
-               Convert.ToString(sendstring);
-               Convert.ToString(optionstring); 
-               sendstring = "Report : A signal has been sent to Menu.cs";
-               optionstring = "This is to inform all crows that the signal is verified and ready to be sent to 'Zero'";
-               
-
-        }
+        
     }
 }
