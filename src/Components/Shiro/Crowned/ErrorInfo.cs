08c06951966e301ab894c3a253c6eb95d7fbcf64
DaVikingMan <shashwatsinha080@gmail.com>
2021-09-12 16:58:04 +0530

    Add foreach loop and create list of type DetailErrorList Create a new list of type DetailErrorList and add a new error Signed-off by : DaVikingMan

--- src/Components/Shiro/Crowned/ErrorInfo.cs
+++ src/Components/Shiro/Crowned/ErrorInfo.cs
@@ -6,19 +6,32 @@ namespace src
 {
     class ErrorInfo
     {
-        public void ErrorInfoContainer(int erroruint)
+        public void ErrorInfoContainer(int erroruint, string signal)
         {
+            List<DetailErrorList> errorLists = ErrorInit.ErrorInitialize();
             Console.ForegroundColor = ConsoleColor.Red;
+            
            
                // This class seems empty because it hasn't been finshed, it should be completed in the next release
-
-             if(erroruint == 20)
+             foreach(var c in errorLists)
+             {
+             if(erroruint == c.Erroruint)
              {
                  
                   Console.WriteLine("The input stream couldn't find a text that matches your input");
                   
                   
            
+             }else if(erroruint == c.Error)
+             {
+                 Console.WriteLine("CS10 : The error occured due to a key-interrupt");
+             }
+             else
+             {
+                 
+                 var errorreceiver = new ErrorReceiver();
+                 errorreceiver.MainReceiver("no", signal);
+             }
              }
              
 
