e9950a46b3e6767cf382350419ec7e3f329164dc
DaVikingMan <shashwatsinha080@gmail.com>
2021-09-24 21:08:17 +0530

    Change to 'CheckInfo()' Signed-off by : DaVikingMan

--- src/Components/Shiro/ErrorInteractor.cs
+++ src/Components/Shiro/ErrorInteractor.cs
@@ -9,12 +9,12 @@ namespace src
               if(errorsignal.Contains('/'))
               {
                   var gray = new Errortunnel();
-                  gray.AddInfo("/", errornum, zerosignal);
+                  gray.CheckInfo("/", errornum, zerosignal);
               }else if(errorsignal.Contains(';'))
               {
                   Console.WriteLine("Not a recongnized symbol, throw new error");
                   Environment.Exit(0);
-                  // This means that the error is not signed by Gray nor Kuroi and should be reported to the issues tab in Github
+                  // This symbol isn't supported and throws a new error, exiting FalconXOS
               }
         }
     }
