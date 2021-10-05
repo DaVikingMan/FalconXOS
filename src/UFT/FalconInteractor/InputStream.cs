209dffc80380404bd1685281970b3cf2a15726ba
DaVikingMan <shashwatsinha080@gmail.com>
2021-10-05 21:16:44 +0530

    Start developement of InputStream ; Implement InputStream.cs This class is the main class for the InputStream system. All things related to InputStream will go to this class. This class's job is to categorize streams into readonly and editable inputstreams(see previous commit for more about readonly and editable) I also forgot to mention in the previous commit that there will a DevConsole which can be used to directly access the InputStream. Signed-off by : DaVikingMan

---
+++ src/UFT/FalconInteractor/InputStream.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Inputstream
+{
+	class InputStream:Falcon.Falcon
+        {
+		public void InputNon() // The non-readable and accessable inputstream.Can only be used for accessing inputstream-non:edit
+		{
+
+		}
+		public void InputEdit() // The editable inputstream which is later converted to Falcon::Input-!edit
+		{
+
+		}
+
+	}
+
+
+}
+
