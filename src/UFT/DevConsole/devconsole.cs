d575c017a6b0880cb65554452d20d560f09ae7e9
DaVikingMan <shashwatsinha080@gmail.com>
2021-10-06 22:16:43 +0530

    Implement devconsole component This is an early implementation of devconsole. The final version of devconsole will use the Inputstream component(as mentioned in the text printed when devconsole is started) The commands used by devconsole will remain the same in the main version of devconsole, only the input system will be changed to Inputstream component Signed-off by : DaVikingMan

--- src/UFT/DevConsole/devconsole.cs
+++ src/UFT/DevConsole/devconsole.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading;
 using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace dev
 {
@@ -8,12 +9,67 @@ namespace dev
     {
          public void MainConsole()
          {
+             Console.WriteLine("devconsole :: Ignore debug for now, all debug messages are for when inputstream will be implemented");
              Console.WriteLine("Input :: ????");
              Console.WriteLine("InputStream Chosen : readonly-edit::");
              Console.WriteLine("Done :: Opened Console:Dev, Console:Input, Receive:Input");
              Thread.Sleep(2000);
              Console.Clear();
-             Console.WriteLine(">> ");
+             Console.Write(">> ");
+	     string consoleread = Console.ReadLine();
+	     if(consoleread == "view @" || consoleread == "view news")
+	     {
+             List<string> commandslist = new List<string>();
+         
+             commandslist.Add("/c cat mainnews.md");
+               var startinfo = new ProcessStartInfo();
+               startinfo.WorkingDirectory = $"{Environment.CurrentDirectory}/Dashboard";
+               startinfo.Arguments = commandslist[0];   
+               
+               	startinfo.FileName = "powershell.exe";
+                   var read = Process.Start(startinfo);
+                //    read.Start();
+              
+                   read.Close();
+                   
+                   Console.ReadKey();
+                  NonStartConsole();
+     
+	     }else if(consoleread == "::exit" || consoleread == "exit" || consoleread == "devconsole::exit")
+         {
+              var Mainc = new src.Main();
+              Mainc.MainStart();
+         }
+
+         }
+         public void NonStartConsole()
+         {
+             Console.Clear();
+              Console.Write(">> ");
+	     string consoleread = Console.ReadLine();
+	     if(consoleread == "view @" || consoleread == "view news")
+	     {
+             List<string> commandslist = new List<string>();
+         
+             commandslist.Add("/c cat mainnews.md");
+               var startinfo = new ProcessStartInfo();
+               startinfo.WorkingDirectory = $"{Environment.CurrentDirectory}/Dashboard";
+               startinfo.Arguments = commandslist[0];   
+               
+               	startinfo.FileName = "powershell.exe";
+                   var read = Process.Start(startinfo);
+                //    read.Start();
+              
+                   read.Close();
+                   
+                   Console.ReadKey();
+                   MainConsole();
+     
+	     }else if(consoleread == "::exit" || consoleread == "exit" || consoleread == "devconsole::exit")
+         {
+              var Mainc = new src.Main();
+              Mainc.MainStart();
+         }
          }
     }
 }
