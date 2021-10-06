6e7610b6fdb725afccf451cb9c5cd14efe4769a3
DaVikingMan <shashwatsinha080@gmail.com>
2021-10-06 11:17:49 +0530

    Add editable inputstream string and add inputstream string to InputEdit Signed-off by : DaVikingMan

--- src/UFT/FalconInteractor/Falcon.cs
+++ src/UFT/FalconInteractor/Falcon.cs
@@ -6,14 +6,21 @@ namespace Falcon
 	{ 
 		public static readonly Falcon obj1 = new Falcon(); // Note this is for readonly
 		public static readonly Falcon obj2 = new Falcon();
+		public static Falcon obj3 = new Falcon(); // Edit stream
+		public static Falcon obj4 = new Falcon();
 
            public static string input;
-          public void MainInput()
+                  public void ReadonlyMainInput()
 		  {
-                Falcon o = Falcon.obj1;
+                                Falcon o = Falcon.obj1;
 				input = Console.ReadLine();
 				input = Convert.ToString(o); // Convert Falcon type to string as the input is a string
-		  }		
+		  }
+		  public void EditInputstream()
+		  {
+			  InputStream input = new InputStream();
+			  input.EditInput
+		  }
 	}
 	class InputStream:Falcon
 	{
@@ -21,6 +28,15 @@ namespace Falcon
 		{
 			Console.WriteLine(input);
 		}
+		public static void EditInput()
+		{
+                       input = Console.ReadLine();
+		       obj3 = input;
+		       var inputstream = new InputStream.InputStream.InputNon();
+		       inputstream(obj3);
+		}
+		
+
 	}
 	
 
