2c2460863a8fd23371c90f508169d9a564633ab7
DaVikingMan <shashwatsinha080@gmail.com>
2021-10-06 22:24:49 +0530

    Change 'Falcon.Falcon' to 'Falcon' and fix errors : remove public readonly Signed-off by : DaVikingMan

--- src/UFT/FalconInteractor/InputStream.cs
+++ src/UFT/FalconInteractor/InputStream.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Inputstream
 {
-	class InputStream:Falcon.Falcon
+	class InputStream:Falcon
         {
 		
 		public void InputNon() // The non-readable and accessable inputstream.Can only be used for accessing inputstream-non:edit
@@ -13,7 +13,7 @@ namespace Inputstream
 		{
 			
 			Console.WriteLine(inputedit);
-                        public readonly string inputread = inputedit; // Change to readonly as there is no use to alter the text.
+                        string inputread = inputedit; // Change to readonly as there is no use to alter the text.
 			// If the text is altered by WritePad, WritePad will use a new inputstream which is a copy of this inputstream
 
 		}
