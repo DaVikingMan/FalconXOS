0e4045a0d00b13a2149ed43d95a5cca0c4ec62f6
DaVikingMan <shashwatsinha080@gmail.com>
2021-10-12 10:59:07 +0700

    Add config rules Not currently used Signed-off by : DaVikingMan

---
+++ src/UFT/config/config/configrules.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace config
+{
+	class configrules
+	{
+		public void config(string config, string config2)
+		{
+			if(config == "Config")
+			{
+				Console.WriteLine("In developement");
+				Console.WriteLine("Config console :: devconsole");
+				Console.WriteLine("\n\nWrite");
+			}
+		}
+	}
+}
