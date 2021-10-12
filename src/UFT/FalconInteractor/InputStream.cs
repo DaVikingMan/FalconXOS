21c48d86aae673c7e84f7a34f1fd6cb3957274d4
DaVikingMan <shashwatsinha080@gmail.com>
2021-10-12 11:02:06 +0700

    Print var Signed-off by : DaVikingMan

--- src/UFT/FalconInteractor/InputStream.cs
+++ src/UFT/FalconInteractor/InputStream.cs
@@ -8,7 +8,8 @@ namespace Inputstream
 		
 		public void InputNon(string inputreadonly) // The non-readable and accessable inputstream.Can only be used for accessing inputstream-non:edit
 		{
-                               
+                               Console.WriteLine("inputreadonly");
+			       
 		}
 		public void InputEdit(string inputedit) // The editable inputstream which is later converted to Falcon::Input-!edit
 		{
@@ -16,7 +17,7 @@ namespace Inputstream
 			Console.WriteLine(inputedit);
                         string inputread = inputedit;
 			// If the text is altered by WritePad, WritePad will use a new inputstream which is a copy of this inputstream
- 
+
 		}
 
 	}
