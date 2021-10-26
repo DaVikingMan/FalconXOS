4b3bb74bcb4553f7c6e0e0e9a0d28665de04462e
DaVikingMan <shashwatsinha080@gmail.com>
2021-10-26 22:33:34 +0700

    Remove quotation marks and add new text Signed-off by : DaVikingMan

--- scripts/install.bat
+++ scripts/install.bat
@@ -1,5 +1,6 @@
 @Echo off
-echo "Installing required packages to run FalconXOS"
+echo Starting installer : chocolatey is the default package manager for installing packages, if you want to use another package manager like winget specify it in this file before running it
+echo Installing packages which are required to run FalconXOS
 choco install dotnet
 choco install openjdk
 choco install nodejs
