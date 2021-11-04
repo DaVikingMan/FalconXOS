54e43f09fec29222862b9f5ed15fbcde5bcdb1f5
DaVikingMan <shashwatsinha080@gmail.com>
2021-11-04 19:18:46 +0700

    Add class for reading doc pages;Add method MainReader Doc pages are kind of like man pages but for FalconXOS This is not the final implementation Signed-off by : DaVikingMan

---
+++ src/Components/DocPages/doccommander.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Diagnostics;
+
+namespace src
+{
+	class docconsole
+	{
+		public void MainReader(string filetoread)
+		{
+		  
+	         	ProcessStartInfo start = new ProcessStartInfo();
+start.Arguments = $"-c \"cat {Environment.CurrentDirectory}/src/Components/DocPages/docpages/{filetoread}.md\"";
+         		start.FileName = "/bin/bash";
+		        start.UseShellExecute = true;
+                	// start.CreateNoWindow = false;
+			Process.Start(start);
+		    
+		}
+	}
+}
