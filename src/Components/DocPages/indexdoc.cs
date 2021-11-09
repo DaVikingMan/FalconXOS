9694efe334d6de484393bcfe96c0b6c017452eb6
DaVikingMan <shashwatsinha080@gmail.com>
2021-11-09 21:52:22 +0700

    Implement IndexList class and methods for seperating command and command arguments Added Indexer() and Communicator() This is an early implementation Signed-off by : DaVikingMan

---
+++ src/Components/DocPages/indexdoc.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace src
+{
+	class IndexList
+	{
+		public void Indexer(int numberofitems)
+		{
+			numberofitems = 2;
+                       string[] items = new string[numberofitems];
+
+		}
+		public void Communicator(string fullstring, string[] stringarray)
+		{
+                var secondstring = fullstring.Split('G');
+				var array = secondstring.ToString();
+				for(int i=0;i <= stringarray.Length;i++)
+				{
+                      if(stringarray[i] == array)
+					  {
+                            
+					  }
+				}
+		}
+
+		
+		
+
+
+	}
+}
