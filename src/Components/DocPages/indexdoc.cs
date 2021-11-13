15724dad84381ad32e255a1c97ce8596389af1ea
DaVikingMan <shashwatsinha080@gmail.com>
2021-11-13 15:11:37 +0700

    Add indexer This is a work in-progress Signed-off by : DaVikingMan

--- src/Components/DocPages/indexdoc.cs
+++ src/Components/DocPages/indexdoc.cs
@@ -4,13 +4,17 @@ namespace src
 {
 	class IndexList
 	{
-		public void Indexer(int numberofitems)
+		public void EmptyMessage()
+		{
+			Indexer(12); // EmptyMessage is an early implementation of a empty termlib which just passes the arguments to the method
+		}
+		public void Indexer(int numberofitems) // This method is called first
 		{
 			numberofitems = 2;
-                       string[] items = new string[numberofitems];
+                       string[] items = new string[numberofitems]; // Stores the files or names of docpages
 
 		}
-		public void Communicator(string fullstring, string[] stringarray)
+		public void Communicator(string fullstring, string[] stringarray) // Indexer calls this method and gives the arguments
 		{
                 var secondstring = fullstring.Split('G');
 				var array = secondstring.ToString();
