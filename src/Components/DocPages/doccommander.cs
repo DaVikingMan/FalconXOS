1dd849bda00a19e4d48d9e94b1fd6723c77fec2a
DaVikingMan <shashwatsinha080@gmail.com>
2021-11-25 21:42:15 +0700

    Return to MainConsole() after output Signed-off by : DaVikingMan

--- src/Components/DocPages/doccommander.cs
+++ src/Components/DocPages/doccommander.cs
@@ -8,12 +8,15 @@ namespace src
 		public void MainReader(string filetoread)
 		{
 		 
-	         	ProcessStartInfo start = new ProcessStartInfo();
+                	ProcessStartInfo start = new ProcessStartInfo();
 start.Arguments = $"-c \"cat {Environment.CurrentDirectory}/src/Components/DocPages/docpages/{filetoread}.docpage\"";
          		start.FileName = "/bin/bash";
 		        start.UseShellExecute = true;
                 	// start.CreateNoWindow = false;
 			Process.Start(start);
+			var devconsole = new dev.DevConsole();
+			Console.ReadKey();
+         		devconsole.MainConsole();
 		    
 		}
 	}
